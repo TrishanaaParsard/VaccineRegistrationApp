b574b7bfe95a28d90bc699683239d93d43ae08b8
TrishanaaParsard <88710723+TrishanaaParsard@users.noreply.github.com>
2021-08-26 14:38:31 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace VaccineRegistrationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.tbxID = new System.Windows.Forms.TextBox();
+            this.tbxFirstName = new System.Windows.Forms.TextBox();
+            this.tbxSurname = new System.Windows.Forms.TextBox();
+            this.cboGender = new System.Windows.Forms.ComboBox();
+            this.CboProvince = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(90, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vaccine Registration";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(92, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "FirstName:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Surname:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(92, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Gender:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(92, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Province:";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(111, 300);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(202, 47);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // tbxID
+            // 
+            this.tbxID.Location = new System.Drawing.Point(213, 86);
+            this.tbxID.Name = "tbxID";
+            this.tbxID.Size = new System.Drawing.Size(121, 22);
+            this.tbxID.TabIndex = 7;
+            // 
+            // tbxFirstName
+            // 
+            this.tbxFirstName.Location = new System.Drawing.Point(213, 152);
+            this.tbxFirstName.Name = "tbxFirstName";
+            this.tbxFirstName.Size = new System.Drawing.Size(121, 22);
+            this.tbxFirstName.TabIndex = 8;
+            // 
+            // tbxSurname
+            // 
+            this.tbxSurname.Location = new System.Drawing.Point(213, 114);
+            this.tbxSurname.Name = "tbxSurname";
+            this.tbxSurname.Size = new System.Drawing.Size(121, 22);
+            this.tbxSurname.TabIndex = 9;
+            // 
+            // cboGender
+            // 
+            this.cboGender.FormattingEnabled = true;
+            this.cboGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cboGender.Location = new System.Drawing.Point(213, 192);
+            this.cboGender.Name = "cboGender";
+            this.cboGender.Size = new System.Drawing.Size(121, 24);
+            this.cboGender.TabIndex = 10;
+            // 
+            // CboProvince
+            // 
+            this.CboProvince.FormattingEnabled = true;
+            this.CboProvince.Items.AddRange(new object[] {
+            "Gauteng",
+            "Northern Cape",
+            "Western Cape",
+            "Eastern Cape",
+            "Free State",
+            "KwaZulu Natal",
+            "Limpopo",
+            "Mpumalanga",
+            "North West"});
+            this.CboProvince.Location = new System.Drawing.Point(213, 222);
+            this.CboProvince.Name = "CboProvince";
+            this.CboProvince.Size = new System.Drawing.Size(121, 24);
+            this.CboProvince.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 370);
+            this.Controls.Add(this.CboProvince);
+            this.Controls.Add(this.cboGender);
+            this.Controls.Add(this.tbxSurname);
+            this.Controls.Add(this.tbxFirstName);
+            this.Controls.Add(this.tbxID);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Vaccine Registration App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.TextBox tbxID;
+        private System.Windows.Forms.TextBox tbxFirstName;
+        private System.Windows.Forms.TextBox tbxSurname;
+        private System.Windows.Forms.ComboBox cboGender;
+        private System.Windows.Forms.ComboBox CboProvince;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaccineRegistrationApp
+{
+    public partial class Form1 : Form
+    {
+        //Create Connection string and connection to SQL
+        private static string myConString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+        SqlConnection con = new SqlConnection(myConString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+           
+            //Open connection to database
+            con.Open();
+
+            
+
+            //Add string with SQL query
+            string InsertQuery = "Insert Into RegTable Values (@ID, @Surname, @FirstName, @Gender, @Province)";
+
+            SqlCommand sqlCommand = new SqlCommand(InsertQuery, con);
+            sqlCommand.Parameters.AddWithValue("@ID", tbxID.Text);
+            sqlCommand.Parameters.AddWithValue("@Surname", tbxSurname.Text);
+            sqlCommand.Parameters.AddWithValue("@FirstName", tbxFirstName.Text);
+            sqlCommand.Parameters.AddWithValue("@Gender", cboGender.Text);
+            sqlCommand.Parameters.AddWithValue("@Province", CboProvince.Text);
+          
+            //Executes SQL query
+            sqlCommand.ExecuteNonQuery();
+            //Message box, informs that the record is created.
+            MessageBox.Show("Successfully created a record!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            clear();
+            con.Close();
+            
+
+        }
+
+        private void clear() {
+            tbxFirstName.Clear();
+            tbxID.Clear();
+            tbxSurname.Clear();
+            
+        }
+    }
+}
